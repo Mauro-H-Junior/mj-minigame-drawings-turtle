cab5565f072afd9b9e0a16e3c8a79ec18009218f
mauro-jr@outlook.com <mauro-jr@outlook.com>
2022-07-17 14:06:20 -0300

    aplicando_funcoes

--- desafioTurtle.py
+++ desafioTurtle.py
@@ -5,75 +5,88 @@ from turtle import Turtle
 
 t = Turtle()
 
+def frente_tras():
+    resultado = int(input('Quantos pixels devem ser percorridos?\n>'))
+    return resultado
+
+def esquerda_direita_frente(tt):
+
+    angulo_valido = False
+    while angulo_valido == False:
+        angulo = input('\E:(esquerda), D:(direita), N:(não rotacionar)?\n>')
+        if angulo == 'D':
+            rotacionar_direita_frente(tt)
+            angulo_valido=True
+           
+        elif angulo == 'E':
+            rotacionar_direita_frente(tt)
+            angulo_valido=True
+        elif angulo =='N':
+            nao_rotacionar_frente(tt)
+            angulo_valido=True
+        else:
+            print('\nOPS, DIGITE E para esquerda ou D para direita')
+
+def esquerda_direita_tras(tt):
+    angulo_valido = False
+    while angulo_valido == False:
+        angulo = input('\E:(esquerda), D:(direita), N:(não rotacionar)?\n>')
+        if angulo == 'D':
+            rotacionar_direita_tras(tt)
+            angulo_valido=True
+           
+        elif angulo == 'E':
+            rotacionar_esquerda_tras(tt)
+            angulo_valido=True
+        elif angulo =='N':
+            nao_rotacionar_tras(tt)
+            angulo_valido=True
+        else:
+            print('\nOPS, DIGITE E para esquerda ou D para direita')
+
+def rotacionar_direita_frente(ttd):
+    pixel_angulo = int(input('Quantos pixels devem ser rotacionados?\n>'))
+    t.right(pixel_angulo)
+    t.forward(pixel_lado)
+
+def rotacionar_direita_tras(ttd):
+    pixel_angulo = int(input('Quantos pixels devem ser rotacionados?\n>'))
+    t.right(pixel_angulo)
+    t.backward(pixel_lado)
+
+def rotacionar_esquerda_frente(tte):
+    pixel_angulo = int(input('Quantos pixels devem ser rotacionados?\n>'))
+    t.left(pixel_angulo)
+    t.forward(pixel_lado)
+
+def rotacionar_esquerda_tras(tte):
+    pixel_angulo = int(input('Quantos pixels devem ser rotacionados?\n>'))
+    t.left(pixel_angulo)
+    t.backward(pixel_lado)
+
+def nao_rotacionar_frente(ttn):
+    t.forward(pixel_lado)
+
+def nao_rotacionar_tras(ttn):
+    t.backward(pixel_lado)
+
+
 while True:
     lado = (input('\nF:(frente), T:(trás)?\n>'))
     
 #-------------------------------------------------      
     if lado == 'F':
         
-        pixel_lado = int(input(
-            'Quantos pixels devem ser percorridos?\n>'))
+        pixel_lado = frente_tras()
 
-        angulo_valido = False
-        while angulo_valido == False:
-
-            angulo = input(
-                '\E:(esquerda), D:(direita), N:(não rotacionar)?\n>')
-                              
-            if angulo == 'D':
-                pixel_angulo = int(input(
-                    'Quantos pixels devem ser rotacionados?\n>'))
-                t.right(pixel_angulo)
-                t.forward(pixel_lado)
-                angulo_valido = True
-                
-                            
-            elif angulo == 'E':
-                pixel_angulo = int(input(
-                    'Quantos pixels devem ser rotacionados?\n>'))
-                t.left(pixel_angulo)
-                t.forward(pixel_lado)
-                angulo_valido = True
-
-            elif angulo =='N':
-                t.forward(pixel_lado) 
-                angulo_valido = True   
-
-            else:
-                print('\nOPS, DIGITE E para esquerda ou D para direita')
-                                        
-           
+        esquerda_direita_frente(t)
+          
 #--------------------------------------------------   
     if lado == 'T':
         
-        pixel_lado = int(input(
-            'Quantos pixels devem ser percorridos?\n>'))
+        pixel_lado = frente_tras()
 
-        angulo_valido = False
-        while angulo_valido == False:
-            angulo = input(
-                '\E:(esquerda), D:(direita), N:(não rotacionar)?\n>')
-                        
-            if angulo == 'D':                
-                pixel_angulo = int(input(
-                    'Quantos pixels devem ser rotacionados?\n>'))
-                t.right(pixel_angulo)
-                t.backward(pixel_lado)
-                angulo_valido = True
-                            
-            elif angulo == 'E':
-                pixel_angulo = int(input(
-                    'Quantos pixels devem ser rotacionados?\n>'))
-                t.left(pixel_angulo)
-                t.backward(pixel_lado)
-                angulo_valido = True
-
-            elif angulo == 'N':
-                t.backward(pixel_lado)
-                angulo_valido = True
-
-            else:
-                print('\nOPS, DIGITE E para esquerda , D para direita ou N para não rotacionar')
+        esquerda_direita_tras(t)
 
     if lado not in ('T','F','N'):
         print('VALOR INVÁLIDO PRESS F:(frente), T:(trás)!\n')
